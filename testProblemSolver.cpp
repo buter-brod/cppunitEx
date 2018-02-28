e181a3705fcdaa067bb91a5c4f1f8997b71c67ee
m.baranov <m.baranov@corp.mail.ru>
2018-02-28 21:38:05 +0300

    makefile added & misc

--- testProblemSolver.cpp
+++ testProblemSolver.cpp
@@ -1,24 +1,25 @@
 #include "problemSolver.h"
 #include <iostream>
-#include "3rdparty/cpptest/src/cpptest.h"
 
-class PSSuite : public Test::Suite {
-	void testSum();
-	void testDivide();
+// class PSSuite : public Test::Suite {
+// 	void testSum();
+// 	void testDivide();
 
-public:
-	PSSuite() {
-		TEST_ADD(PSSuite::testSum);
-		TEST_ADD(PSSuite::testDivide);
-	}
-};
+// public:
+// 	PSSuite() {
+// 		TEST_ADD(PSSuite::testSum);
+// 		TEST_ADD(PSSuite::testDivide);
+// 	}
+// };
 
 
 int main() {
 
 	std::cout << "Tests run: \n";
 
-	PSSuite tests;
-	Test::TextOutput output(Test::TextOutput::Verbose);
-	return tests.run(output);
+	return 0;
+
+//	PSSuite tests;
+//	Test::TextOutput output(Test::TextOutput::Verbose);
+//	return tests.run(output);
 }
