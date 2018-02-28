b3638cf89a393d9886f94369db5c226b2b9963e7
m.baranov <m.baranov@corp.mail.ru>
2018-03-01 02:14:48 +0300

    test cases

--- Makefile
+++ Makefile
@@ -1,10 +1,7 @@
-all: project tests
+all: project
 
 project:
 	g++ main.cpp problemSolver.cpp -o problemSolver
-
-tests:
-	g++ testProblemSolver.cpp -o tests
 
 clean:
 	rm -rf *.o tests problemSolver
--- testProblemSolver.cpp
+++ testProblemSolver.cpp
@@ -1,25 +1,48 @@
 #include "problemSolver.h"
 #include <iostream>
 
-// class PSSuite : public Test::Suite {
-// 	void testSum();
-// 	void testDivide();
+#include <cppunit/ui/text/TextTestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
 
-// public:
-// 	PSSuite() {
-// 		TEST_ADD(PSSuite::testSum);
-// 		TEST_ADD(PSSuite::testDivide);
-// 	}
-// };
+class TestProblemSolver : public CppUnit::TestCase {
+public:
+	
+	void checkSum() {
+		CPPUNIT_ASSERT_MESSAGE("sum is not 30 :(", ProblemSolver::sum(10, 20) == 30);
+	}
+	
+	void checkDivide() {
+		CPPUNIT_ASSERT_MESSAGE("expected 3 :(", ProblemSolver::divide(16, 5) == 3);
+	}
+	
+	void checkDivideFailure() {
+		
+		try {
+			ProblemSolver::divide(100, 0);
+		} catch(...) {
+			return;
+		}
+		
+		CPPUNIT_FAIL("ProblemSolver::divide did not throw division by zero");
+	}
+	
+	CPPUNIT_TEST_SUITE(TestProblemSolver);
+	CPPUNIT_TEST(checkSum);
+	CPPUNIT_TEST(checkDivide);
+	CPPUNIT_TEST(checkDivideFailure);
+	CPPUNIT_TEST_SUITE_END();
+};
 
+CPPUNIT_TEST_SUITE_REGISTRATION(TestProblemSolver);
 
 int main() {
 
 	std::cout << "Tests run: \n";
-
+	
+	CppUnit::Test *test = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
+	CppUnit::TextTestRunner runner;
+	runner.addTest(test);
+	runner.run();
 	return 0;
-
-//	PSSuite tests;
-//	Test::TextOutput output(Test::TextOutput::Verbose);
-//	return tests.run(output);
 }
